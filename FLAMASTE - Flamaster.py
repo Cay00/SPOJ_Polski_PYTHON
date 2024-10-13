26e1f8c7487971d6c9d606e39fb98a6f5c020e4e
Krzysztof <krzysztof.turek007@gmail.com>
2024-10-13 22:27:22 +0200

    Solution

---
+++ FLAMASTE - Flamaster.py
@@ -0,0 +1,19 @@
+import string
+
+test = int(input())
+
+for _ in range(test):
+    input_data = input()
+    counter = 1
+
+    for i in range(1, len(input_data) + 1):
+        if i == len(input_data) or input_data[i] != input_data[i - 1]:
+            if counter > 2:
+                print(input_data[i - 1], counter, sep='', end='')
+            elif counter == 2:
+                print(input_data[i - 1] * 2, end='')
+            else:
+                print(input_data[i - 1], end='')
+            counter = 1
+        else:
+            counter += 1
