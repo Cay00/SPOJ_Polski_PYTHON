a598d98dd1f3fcbc0995da1565aebd071088a80d
Krzysztof <krzysztof.turek007@gmail.com>
2024-10-14 10:44:07 +0200

    Solution

--- AL_05_01 - Czarodziejskie lusterko.py
+++ AL_05_01 - Czarodziejskie lusterko.py
@@ -1,14 +1,20 @@
+def lustrzane_odbicie(num_dec):
+    num_bin = bin(num_dec)[2:]  # Konwersja liczby dziesiętnej na binarną
+    num_bin_2 = num_bin[::-1]    # Odwrócenie binarnej reprezentacji
+    num_dec_2 = int(num_bin_2, 2)  # Konwersja odwróconej binarnej na dziesiętną
+    return num_dec_2
+
+data = []
 while True:
+    input_data = input()
+    if input_data == "":  # Przerywamy wczytywanie, gdy użytkownik poda pusty ciąg
+        break
     try:
-        data_in = input()
-        if data_in == "":
-            break
-        number_dec = int(data_in)
-        number_bin = bin(number_dec)[2:]
-        number_dec_2 = number_bin[::-1]
-        number_bin_2 = int(number_dec_2, 2)
-
-        print(number_bin_2)
-
+        number = int(input_data)  # Próbujemy zamienić wejście na liczbę całkowitą
+        data.append(number)        # Dodajemy liczbę do listy
     except ValueError:
         break
+
+for number in data:
+    output = lustrzane_odbicie(number)
+    print(output)
